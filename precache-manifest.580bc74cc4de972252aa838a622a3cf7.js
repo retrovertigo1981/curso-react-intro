d00f3dee10e9026528ee425544954bae0d65591b
Sebastián Peña <fuzzdev@Sebastian>
2023-07-28 17:13:24 -0400

    Updates

---
+++ precache-manifest.580bc74cc4de972252aa838a622a3cf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2bab2d3dc7e13514709",
+    "url": "./static/css/main.e2894690.chunk.css"
+  },
+  {
+    "revision": "f2bab2d3dc7e13514709",
+    "url": "./static/js/main.f2bab2d3.chunk.js"
+  },
+  {
+    "revision": "480a353721bd092f9455",
+    "url": "./static/js/1.480a3537.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "8fa75a6a524cc49d53b5dbea74855647",
+    "url": "./static/media/check-icon.8fa75a6a.svg"
+  },
+  {
+    "revision": "b0316cb974797f13c419ee69742ffc86",
+    "url": "./static/media/delete-icon.b0316cb9.svg"
+  },
+  {
+    "revision": "6b06fd38e18c8f562ac3bdd5f1a2f08f",
+    "url": "./index.html"
+  }
+];
